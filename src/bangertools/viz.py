fa1d819855a3650e306448d4d652bcb5e467929c
Stryder <stryder@strangedreams.com>
2026-06-19 23:29:19 -0400

    Refactor Viz commands

--- src/bangertools/viz.py
+++ src/bangertools/viz.py
@@ -1,28 +1,25 @@
 import typer
 
+import bangertools.lib.viewer as viewer
 from bangertools import FilePath
 
 app = typer.Typer(help="Data visualization commands")
 
 
-@app.command(name="npz")
-def view_core_command(
+@app.command(name="build_npz")
+def build_npz_command(
         file_path: FilePath = "./core_trajectories.npz"):  # TODO: Make this a directory and a file and default to local and this name for either if they're not present.
-    from bangertools.lib.core import view_core
-    view_core(file_path)
+    viewer.view_core(file_path)
+
+
+@app.command(name="view_npz")
+def view_npz_command(npz_path: FilePath = "./core_trajectories.npz"):
+    return viewer.collapse_viewer(dir_path=npz_path)
 
 
 @app.command(name="render")
-def render_command(mode: int, dir_path: str, npz_path="./core_trajectories.npz"):
-    import bangertools.lib.viewer as viewer
-
-    match mode:
-        case 1:
-            return viewer.collapse_viewer(dir_path=npz_path),
-        case 2:
-            return viewer.render2(dir_path)
-        case _:
-            print("unrecognized mode")
+def render_command(dir_path: FilePath = "./"):
+    return viewer.render2(dir_path)
 
 
 if __name__ == "__main__":
