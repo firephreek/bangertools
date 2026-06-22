2487cdd6417260db2eea3fb75fe4ce9b2c757887
Stryder <stryder@strangedreams.com>
2026-06-21 23:26:46 -0400

    Move Renderer class to common.py

--- src/bangertools/viz/common.py
+++ src/bangertools/viz/common.py
@@ -1,5 +1,26 @@
+import pickle
+from pathlib import Path
+
 import numpy as np
 from matplotlib import cm
+
+
+class Renderer:
+
+    def __init__(self, dir_path, extension, rebuild_cache=False):
+
+        dir_path = Path(dir_path).resolve()
+        cache_file_path = dir_path / f"{dir_path.name}.frames.{extension}"
+
+        try:
+            with open(cache_file_path, 'rb') as fp:
+                self.frames = pickle.load(fp)
+            if self.frames is None or len(self.frames) == 0 or rebuild_cache:
+                raise Exception()
+        except Exception as _:
+            self.frames = self.load_frames(dir_path)
+            with open(cache_file_path, 'wb') as fp:
+                pickle.dump(self.frames, fp)
 
 
 def color_from_temperature(temp):
--- src/bangertools/viz/generic_renderer.py
+++ src/bangertools/viz/generic_renderer.py
@@ -1,29 +1,10 @@
-import pickle
 import re
 from pathlib import Path
 
 import numpy as np
 import pynbody
 
-from .common import color_from_temperature
-
-
-class Renderer:
-
-    def __init__(self, dir_path, extension, rebuild_cache=False):
-
-        dir_path = Path(dir_path).resolve()
-        cache_file_path = dir_path / f"{dir_path.name}.frames.{extension}"
-
-        try:
-            with open(cache_file_path, 'rb') as fp:
-                self.frames = pickle.load(fp)
-            if self.frames is None or len(self.frames) == 0 or rebuild_cache:
-                raise Exception()
-        except Exception as _:
-            self.frames = self.load_frames(dir_path)
-            with open(cache_file_path, 'wb') as fp:
-                pickle.dump(self.frames, fp)
+from .common import color_from_temperature, Renderer
 
 
 class GenericRenderer(Renderer):
