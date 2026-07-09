727b19d0381e3127a6baee25eb51e0e72ec0d643
Stryder <stryder@strangedreams.com>
2026-07-09 00:58:38 -0400

    Add --no-source argument to exclude the source column from exports Updated docs/help, made parameters explicit.

--- src/bangertools/main.py
+++ src/bangertools/main.py
@@ -3,7 +3,7 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 import pynbody
 import typer
-from typer import Typer, Option, Argument
+from typer import Typer, Option
 
 from bangertools import PathList
 from bangertools.ahf.app import ahf_app
@@ -25,9 +25,13 @@ def main(verbose: bool = typer.Option(False, "--verbose", "-v"), ):
 
 @app.command("parq")
 def save_data_to_parq_file(file_paths: PathList,
-                           out: str = Argument(help="File name to save the extracted data to."),
-                           cols: str = Option(..., "--cols", "-c", help="columns to extract"),
-                           ignore_missing_cols: bool = False):
+                           out: str = Option(..., "--out", "-o", help="File name to save the extracted data to"),
+                           cols: str = Option(..., "--cols", "-c",
+                                              help="The columns to be extracted into the new file"),
+                           exclude_source: bool = Option(False, "--no-source",
+                                                         help="If set, the source column will not be added to the export"),
+                           ignore_missing_cols: bool = Option(False, "--ignore-missing-cols",
+                                                              help="Snapshots that are missing columns will be skipped unless this is set.")):
     """
     Extract a set of columns from all snapshots found in the provided paths and write them
     to a Pandas-compatible Parquet file.
@@ -38,6 +42,7 @@ def save_data_to_parq_file(file_paths: PathList,
     writer = None
 
     try:
+        util.verbose(f"Loading snapshots from '{snapshot_paths}'")
         for path in snapshot_paths:
             util.verbose(f"Loading {path}")
             sim = pynbody.load(path)
@@ -57,10 +62,11 @@ def save_data_to_parq_file(file_paths: PathList,
                 for col in cols
             })
 
-            table = table.append_column(
-                "source",
-                pa.array([path.name] * table.num_rows)
-            )
+            if exclude_source:
+                table = table.append_column(
+                    "source",
+                    pa.array([path.name] * table.num_rows)
+                )
 
             if writer is None:
                 if appstate.verbose:
